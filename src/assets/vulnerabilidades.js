cba95fb1cd6fa9cbcbac256f042dc77c5893b503
jvelazquezm <j.velazquezm@alumnos.upm.es>
2022-02-15 13:49:31 +0100

    Cambios de diseño

---
+++ src/assets/vulnerabilidades.js
@@ -0,0 +1,51 @@
+exports.vulnerabilidades = [
+    {
+        "categorias":["Firmware","Comunicaciones","Categoría", "Tratamiento de datos", "Interfaz física", "Accesibilidad"],
+        "popup":[
+            "Ver consejo de buenas prácticas de seguridad",
+            "Debes actualizar tu dispositivo cuanto antes",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "Revisa los ajustes de privacidad de tu dispositivo para comprobar que datos está enviando a la nube",
+            "No permitas que nadie desconocido inserte una memoria USB en tu dispositivo",
+            "",
+            ["Cubre la cámara o apaga el dispositivo cuando no necesites de su uso","Silencia el dispositivo o apágalo cuando no necesites de su uso. No situes otros dispositivos que reproduzcan audio cerca de tu dispositivo"],
+            "",
+            "",
+            "Si es posible, desactiva el acceso remoto en caso de que no vayas a utilizarlo",
+            "Si es posible, actívalo en los ajustes de tu dispositivo",
+            "",
+            "Evita acceder via web a la aplicación en la medida de lo posible"
+        ],
+        "warning":[
+            "No se han encontrado vulnerabilidades en este apartado",
+            "¡Cuidado! Tu dispositivo se encuentra desactualizado.",
+            "¡Cuidado! Tu dispositivo emplea el protocolo de comunicación RFID, el cuál resulta muy inseguro ante ataques físicos y a las etiquetas que utilizan.",
+            "¡Cuidado! Tu dispositivo no cuenta con protocolos de seguridad, la información que envías y que recibes puede estar en peligro y ser obtenida por terceras personas.",
+            "¡Cuidado! Ten cuenta que tu dispositivo se conecta directamente a la red de tu hogar por lo que es más probable que sea objetivo de ataques que buscan infiltrarse en tu red.",
+            "",
+            "",
+            "¡Cuidado! Ten en cuenta que tu dispositivo envía datos a la nube por lo que trata de darle al dispositivo la información estrictamente necesaria para su funcionamiento.",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+
+        ]
+
+}
+
+]
--- src/components/Encabezado.js
+++ src/components/Encabezado.js
@@ -1,7 +1,23 @@
-export default function Encabezado(props) {
-	
-	return <div className="navbar">
-		<i className="fa fa-question-circle"></i>
-		App Metodología
-	</div>
+import React from 'react';
+
+export default class Encabezado extends React.Component {
+	render(){
+		const {comienzo,terminado, enviado,index, titulos} = this.props;
+
+		return <div className="navbar">
+			<i className="fas fa-question-circle"></i>
+			{comienzo === false ? titulos[6] 
+			:
+			terminado === false ? titulos[7] + (index + 1)
+			: 
+			enviado   === false ? titulos[8]
+			:
+			titulos[9]
+			
+			}
+			
+			
+		</div>
+	}
+
 }
